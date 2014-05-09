b7ce7067d30ca45f912bbb8613070042b15d2831
Jonas Pommerening <jonas.pommerening@aixigo.de>
2014-05-09 17:00:33 +0200

    JSHint

--- tasks/lib/load.js
+++ tasks/lib/load.js
@@ -3,14 +3,13 @@
  * Released under the MIT license.
  * http://laxarjs.org/license
  */
-/*jshint node: true*/
 module.exports = function loadNpmTasks( grunt, module ) {
    'use strict';
 
    var path = require( 'path' );
    var findup = require( 'findup' );
    var tasks = path.join( 'node_modules', module, 'tasks' );
-   var module = findup.sync( __dirname, tasks );
+   var plugin = findup.sync( __dirname, tasks );
 
-   grunt.loadTasks( path.join( module, tasks ) );
+   grunt.loadTasks( path.join( plugin, tasks ) );
 };
--- tasks/portal_angular_dependencies.js
+++ tasks/portal_angular_dependencies.js
@@ -3,7 +3,6 @@
  * Released under the MIT license.
  * http://laxarjs.org/license
  */
-/*jshint node: true*/
 module.exports = function( grunt ) {
    'use strict';
 
@@ -81,7 +80,7 @@ module.exports = function( grunt ) {
                grunt.file.write( file.dest, generateBootstrapCode( dependencies.requires ) );
                grunt.log.ok( 'Created Angular dependencies in "' + file.dest + '".' );
                done();
-            } ).catch( grunt.fail.fatal )
+            } ).catch( grunt.fail.fatal );
          }, done );
       } );
 };
