839e592a85245577c1eefc0622a82d3e5aae979c
Michael Kurze <michael.kurze@aixigo.de>
2015-09-10 15:58:35 +0200

    reviving upgrade_pages task

--- tasks/deprecated/upgrade_pages.js
+++ tasks/deprecated/upgrade_pages.js
@@ -9,7 +9,7 @@ module.exports = function( grunt ) {
    var minimatch = require( 'minimatch' );
    var path = require( 'path' );
 
-   var object;
+   var ax = laxarMock();
    var rules;
 
    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -52,7 +52,7 @@ module.exports = function( grunt ) {
          var matches = deepEnumerate( instance.features );
          Object.keys( matches ).filter( minimatchPredicate( rule.pattern ) ).forEach( function( path ) {
             var parentPath = path.substring( 0, path.lastIndexOf( '.' ) );
-            var parent = object.path( instance.features, parentPath );
+            var parent = ax.object.path( instance.features, parentPath );
             var fromName = path.substring( path.lastIndexOf( '.' ) + 1 );
             grunt.log.writeln( 'Upgrade:      RENAME: (' + instance.widget +  '):' +
                parentPath + '[' + fromName + ' -> ' + rule.to + ']' );
@@ -66,12 +66,12 @@ module.exports = function( grunt ) {
          var matches = deepEnumerate( instance.features );
          Object.keys( matches ).filter( minimatchPredicate( rule.pattern ) ).forEach( function( path ) {
             var parentPath = path.substring( 0, path.lastIndexOf( '.' ) );
-            var child = object.path( instance.features, path );
-            var wrapper = object.path( instance.features, parentPath );
+            var child = ax.object.path( instance.features, path );
+            var wrapper = ax.object.path( instance.features, parentPath );
             if( Object.keys( wrapper ).length === 1 ) {
                grunt.log.writeln( 'Upgrade:      UNWRAP_SINGLE: (' + instance.widget +  '):' +
                   parentPath + '[' + path.substring( path.lastIndexOf( '.' ) + 1 ) + ' -> .]' );
-               object.setPath( instance.features, parentPath, child );
+               ax.object.setPath( instance.features, parentPath, child );
             }
          } );
          return instance;
@@ -81,14 +81,14 @@ module.exports = function( grunt ) {
          var matches = deepEnumerate( instance.features );
          Object.keys( matches ).filter( minimatchPredicate( rule.pattern ) ).forEach( function( path ) {
             var parentPath = path.substring( 0, path.lastIndexOf( '.' ) );
-            var child = object.path( instance.features, path );
-            var parent = object.path( instance.features, parentPath );
+            var child = ax.object.path( instance.features, path );
+            var parent = ax.object.path( instance.features, parentPath );
             var fromName = path.substring( path.lastIndexOf( '.' ) + 1 );
             var newParentPath = rule.to.substring( 0, rule.to.lastIndexOf( '.' ) );
             var newKey = rule.to.substring( rule.to.lastIndexOf( '.' ) + 1 );
             grunt.log.writeln( 'Upgrade:      MOVE: (' + instance.widget +  '): ' +
                parentPath + '.' + fromName + ' -> ' + newParentPath + '.' + newKey );
-            object.setPath( instance.features, rule.to, child );
+            ax.object.setPath( instance.features, rule.to, child );
             delete parent[ fromName ];
             if( Object.keys( instance.features[ parentPath ] ).length  === 0 ) {
                delete instance.features[ parentPath ];
@@ -123,7 +123,7 @@ module.exports = function( grunt ) {
       if( !widget || !( widget in rules ) ) {
          return instance;
       }
-      grunt.verbose.writeln( 'Upgrade ATP33>34:    - ' + instance.widget );
+      grunt.verbose.writeln( 'Upgrade:    - ' + instance.widget );
       return rules[ widget ].reduce( applyRule, instance );
    }
 
@@ -133,13 +133,13 @@ module.exports = function( grunt ) {
       if( 'areas' in page ) {
          // page, composition
          Object.keys( page.areas ).forEach( function( areaName ) {
-            grunt.verbose.writeln( 'Upgrade ATP33>34: >> "' + pagePath + '"#' + areaName );
+            grunt.verbose.writeln( 'Upgrade: >> "' + pagePath + '"#' + areaName );
             page.areas[ areaName ] = page.areas[ areaName ].map( transformWidgetInstance );
          } );
       }
       if( 'widgets' in page ) {
          // mixin
-         grunt.verbose.writeln( 'Upgrade ATP33>34: >> "' + pagePath + '"#widgets' );
+         grunt.verbose.writeln( 'Upgrade: >> "' + pagePath + '"#widgets' );
          page.widgets = page.widgets.map( transformWidgetInstance );
       }
       if( 'extends' in page ) {
@@ -156,28 +156,28 @@ module.exports = function( grunt ) {
 
       var backupPath;
       if( mode === 'revert' ) {
-         grunt.verbose.writeln( 'Upgrade ATP33<34: >> "' + pagePath + '": restoring backup file' );
+         grunt.verbose.writeln( 'Upgrade: >> "' + pagePath + '": restoring backup file' );
          backupPath = pagePath;
          pagePath = backupPath.replace( /(.*)[.]backup.json$/, '$1.json' );
          grunt.file.copy( backupPath, pagePath );
          grunt.file[ 'delete' ]( backupPath );
       }
       else if( mode === 'prune' ) {
-         grunt.log.writeln( 'Upgrade ATP33>34: >> "' + pagePath + '": pruning backup file' );
+         grunt.log.writeln( 'Upgrade: >> "' + pagePath + '": pruning backup file' );
          grunt.file[ 'delete' ]( pagePath );
       }
       else {
          if( pagePath.indexOf( BACKUP_SUFFIX ) === pagePath.length - BACKUP_SUFFIX.length ) {
-            grunt.verbose.writeln( 'Upgrade ATP33>34: >> "' + pagePath + '": skipping backup file' );
+            grunt.verbose.writeln( 'Upgrade: >> "' + pagePath + '": skipping backup file' );
             return;
          }
          backupPath = pagePath.replace( /(.*)[.]json$/, '$1' + BACKUP_SUFFIX );
          if( grunt.file.exists( backupPath ) ) {
             // do not overwrite existing backup:
-            grunt.verbose.writeln( 'Upgrade ATP33>34: >> "' + pagePath + '": backup already exists' );
+            grunt.verbose.writeln( 'Upgrade: >> "' + pagePath + '": backup already exists' );
          }
          else {
-            grunt.verbose.writeln( 'Upgrade ATP33>34: >> "' + pagePath + '": creating backup: ', backupPath );
+            grunt.verbose.writeln( 'Upgrade: >> "' + pagePath + '": creating backup: ', backupPath );
             grunt.file.copy( pagePath, backupPath );
          }
 
@@ -202,10 +202,8 @@ module.exports = function( grunt ) {
             requireConfig: 'require_config.js'
          } );
 
-         var config = require( '../lib/require_config' )( options.requireConfig, options );
+         var config = require( '../../lib/require_config' )( options.requireConfig, options );
          var requirejs = require( 'requirejs' ).config( config );
-         var objectUrl = options.laxar + '/lib/utilities/object';
-         object = requirejs( objectUrl );
 
          rules = options.rules;
 
@@ -213,11 +211,90 @@ module.exports = function( grunt ) {
          var target = this.target;
          fileSets.forEach( function( fileSet ) {
             fileSet.src.forEach( function( file ) {
-               grunt.verbose.writeln( 'Upgrade ATP33>34: Page "' + file + '"' );
+               grunt.verbose.writeln( 'Upgrade: Page "' + file + '"' );
                var pathToPages =
                   requirejs.toUrl( options.pages ).replace( requirejs.toUrl( options.appRoot ) + '/', '' );
                processPagePath( '' + file, target, pathToPages );
             } );
          } );
       } );
+
+   /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+   function laxarMock() {
+
+      return  {
+         object: {
+            setPath: setPath,
+            path: getPath
+         }
+      };
+
+      function getPath( obj, thePath, optionalDefault ) {
+         var defaultResult = arguments.length === 3 ? optionalDefault : undefined;
+
+         var pathArr = thePath.split( '.' );
+         var node = obj;
+         var key = pathArr.shift();
+
+         while( key ) {
+            if( node && typeof node === 'object' && hasOwnProperty( node, key ) ) {
+               node = node[ key ];
+               key = pathArr.shift();
+            }
+            else {
+               return defaultResult;
+            }
+         }
+
+         return node;
+      }
+
+      //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      function setPath( obj, path, value ) {
+         var node = obj;
+         var pathArr = path.split( '.' );
+         var last = pathArr.pop();
+
+         pathArr.forEach( function( pathFragment, index ) {
+            if( !node[ pathFragment ] || typeof node[ pathFragment ] !== 'object' ) {
+               var lookAheadFragment = pathArr[ index + 1 ] || last;
+               if( lookAheadFragment.match( /^[0-9]+$/ ) ) {
+                  node[ pathFragment ] = [];
+                  fillArrayWithNull( node[ pathFragment ], parseInt( lookAheadFragment, 10 ) );
+               }
+               else {
+                  node[ pathFragment ] = {};
+               }
+            }
+
+            node = node[ pathFragment ];
+         } );
+
+         if( Array.isArray( node ) && last > node.length ) {
+            fillArrayWithNull( node, last );
+         }
+
+         node[ last ] = value;
+
+         return obj;
+      }
+
+      //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      function fillArrayWithNull( arr, toIndex ) {
+         for( var i = arr.length; i < toIndex; ++i ) {
+            arr[ i ] = null;
+         }
+      }
+
+      //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+      function hasOwnProperty( object, property ) {
+         return Object.prototype.hasOwnProperty.call( object, property );
+      }
+
+   }
+
 };
